f4e8a3f867b29fc07196b32c0f7600314e964cf8
buqiaodaimazzz <58408257+buqiaodaimazzz@users.noreply.github.com>
2020-04-09 12:36:02 +0800

    login
    login 1

--- src/router/index.js
+++ src/router/index.js
@@ -21,13 +21,34 @@ Vue.use(VueRouter)
     component: Center
   },
   {
+    path:"/login",
+    component:() => import("@/views/Login")
+  },
+  {
     path: '/info',
     component: Info
   },
+  {
+    path:"/login",
+    component:() => import("@/views/Login")
+  }
 ]
 
 const router = new VueRouter({
+  mode:"hash", // 默认采用hash模式
   routes
 })
 
+
+router.beforeEach((to, from, next) => {
+  if (to.path === "/center") {
+    if (localStorage.getItem("token")) { // 说明用户已经登录了
+      next()
+    } else {
+      next("/login") // 如果用户没有登录直接跳转到登录界面进行用户登录
+    }
+  } else {
+    next()
+  }
+})
 export default router
